fa7d3f419e3b22af81d4e45db8e86be68cd3f92c
Christy Perozzi <christy.perozzi@gmail.com>
2021-11-16 12:36:51 -0500

    commit tree c13e7fbc8efd57a88fb1a000a065cb897afe167c

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]/g, "");
+  } 
+
+  static titleize(string){
+    let array = string.split(' '); 
+    let newArray = []; 
+    let lowerWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    newArray.push(array[0].charAt(0).toUpperCase() + array[0].slice(1).toLowerCase())
+    array.shift()
+
+    array.forEach( x => {
+      if (lowerWords.includes(x.toLowerCase())){
+        newArray.push(x.toLowerCase());
+      }
+      else {
+        newArray.push(x.charAt(0).toUpperCase() + x.slice(1).toLowerCase());
+      }
+
+    })
+
+    return newArray.join(" "); 
+  }
+
 }
+
+let newString = Formatter.titleize("wHere THe wilD things are")
+//Formatter.capitalize('test')
+//newString = Formatter.sanitize('Amy-Lou x-A12 @#$.com')
+console.log(newString); 
+
